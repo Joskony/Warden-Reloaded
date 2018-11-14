04e05214cf3059ae17f52d6c30e142719a515bfa
Konrad Mampe <konradmampe@ws-79-109.haw-1x.haw-hamburg.de>
2018-11-14 15:29:44 +0100

    Basic movement on buttons

---
+++ Assets/Scripts/Shields/ShieldManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldManager : MonoBehaviour
+{
+
+	[SerializeField] private float m_rotationSpeed = 250.0f;
+	public float m_rotationDirection;
+
+	private void FixedUpdate()
+	{
+		ControlShields();
+	}
+
+	private void ControlShields()
+	{
+		float rotationDir = m_rotationDirection * m_rotationSpeed;
+		rotationDir *= Time.deltaTime;
+		transform.Rotate(0, 0, rotationDir);
+	}
+
+	public void SetRotationDirection(float i)
+	{
+		m_rotationDirection = i;
+	}
+}
---
+++ Assets/Scripts/UI/MovementButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class ShieldMovementEvent : UnityEvent<float>{}
+
+public class MovementButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+	public ShieldManager m_shieldManager;
+	
+	public float m_rotationDirection;
+	private ShieldMovementEvent m_shieldMovementEvent;
+	private float m_startingRotationDirection;
+
+	private void Start()
+	{
+		m_startingRotationDirection = m_rotationDirection;
+		
+		if (m_shieldMovementEvent == null)
+			m_shieldMovementEvent = new ShieldMovementEvent();
+		
+		m_shieldMovementEvent.AddListener(
+			delegate
+			{
+				m_shieldManager.SetRotationDirection(m_rotationDirection);
+			}	
+		);
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{	
+		m_rotationDirection = 0;
+		m_shieldMovementEvent.Invoke(m_rotationDirection);
+		Debug.Log("Up!");
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{	
+		m_rotationDirection = m_startingRotationDirection;
+		m_shieldMovementEvent.Invoke(m_rotationDirection);
+		Debug.Log("Turn!");
+	}
+}
