e07d20cc4d29e094e1514f4455875d3041ab7c68
Konrad <Joskony@users.noreply.github.com>
2018-11-15 00:37:11 +0100

    Swipe with correct behaviour

--- Assets/Scripts/UI/BombInput.cs
+++ Assets/Scripts/UI/BombInput.cs
@@ -4,38 +4,37 @@ namespace UI
 {
 	public class BombInput : MonoBehaviour
 	{
-		private const float M_MAX_SWIPE_TIME = 0.5f; 
+		private const float M_MAX_SWIPE_TIME = 0.25f; 
 		private const float M_MIN_SWIPE_DISTANCE = 0.15f;
+		private const float M_MAX_SWIPE_DISTANCE = 0.3f;
 
 		private Vector2 m_startingPosition;
 		private float m_startingTime;
-
-		public GameObject player;
 		
 		public void Update()
 		{
 			if (Input.touches.Length <= 0) return;
-			
-			Touch touch = Input.GetTouch(0);
-			
-			if(touch.phase == TouchPhase.Began)
+
+			foreach (var touch in Input.touches)
 			{
-				m_startingPosition = new Vector2(touch.position.x / Screen.width, touch.position.y / Screen.width);
-				m_startingTime = Time.time;
-			}
+				if(touch.phase == TouchPhase.Began)
+				{
+					m_startingPosition = new Vector2(touch.position.x / Screen.width, touch.position.y / Screen.width);
+					m_startingTime = Time.time;
+				}
 			
-			if(touch.phase == TouchPhase.Ended)
-			{
-				if (Time.time - m_startingTime > M_MAX_SWIPE_TIME) return;
+				if(touch.phase == TouchPhase.Ended)
+				{
+					if (Time.time - m_startingTime > M_MAX_SWIPE_TIME) return;
 
-				Vector2 endingPosition = new Vector2(touch.position.x / Screen.width, touch.position.y / Screen.width);
+					Vector2 endingPosition = new Vector2(touch.position.x / Screen.width, touch.position.y / Screen.width);
 
-				Vector2 swipe = new Vector2(endingPosition.x - m_startingPosition.x, endingPosition.y - m_startingPosition.y);
+					Vector2 swipe = new Vector2(endingPosition.x - m_startingPosition.x, endingPosition.y - m_startingPosition.y);
 
-				if (swipe.magnitude < M_MIN_SWIPE_DISTANCE) return;
+					if (swipe.magnitude < M_MIN_SWIPE_DISTANCE || swipe.magnitude > M_MAX_SWIPE_DISTANCE) return;
 
-				// Swipe action here
-				player.SetActive(false);
+					// Write action to be executed on swipe below
+				}
 			}
 		}
 	}
