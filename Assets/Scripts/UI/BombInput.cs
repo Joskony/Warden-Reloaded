fdf21f2591e32814d8ebc928ac061a3d82f2933a
Konrad <Joskony@users.noreply.github.com>
2018-11-16 12:59:30 +0100

    Minor refactoring

--- Assets/Scripts/UI/BombInput.cs
+++ Assets/Scripts/UI/BombInput.cs
@@ -4,14 +4,14 @@ namespace UI
 {
 	public class BombInput : MonoBehaviour
 	{
-		private const float M_MAX_SWIPE_TIME = 0.25f; 
-		private const float M_MIN_SWIPE_DISTANCE = 0.15f;
-		private const float M_MAX_SWIPE_DISTANCE = 0.3f;
+		private const float M_MAX_FLICK_TIME = 0.25f; 
+		private const float M_MIN_FLICK_DISTANCE = 0.15f;
+		private const float M_MAX_FLICK_DISTANCE = 0.3f;
 
 		private Vector2 m_startingPosition;
 		private float m_startingTime;
 		
-		public void Update()
+		private void Update()
 		{
 			if (Input.touches.Length <= 0) return;
 
@@ -25,15 +25,15 @@ namespace UI
 			
 				if(touch.phase == TouchPhase.Ended)
 				{
-					if (Time.time - m_startingTime > M_MAX_SWIPE_TIME) return;
+					if (Time.time - m_startingTime > M_MAX_FLICK_TIME) return;
 
 					Vector2 endingPosition = new Vector2(touch.position.x / Screen.width, touch.position.y / Screen.width);
 
-					Vector2 swipe = new Vector2(endingPosition.x - m_startingPosition.x, endingPosition.y - m_startingPosition.y);
+					Vector2 flick = new Vector2(endingPosition.x - m_startingPosition.x, endingPosition.y - m_startingPosition.y);
 
-					if (swipe.magnitude < M_MIN_SWIPE_DISTANCE || swipe.magnitude > M_MAX_SWIPE_DISTANCE) return;
+					if (flick.magnitude < M_MIN_FLICK_DISTANCE || flick.magnitude > M_MAX_FLICK_DISTANCE) return;
 
-					// Write action to be executed on swipe below
+					// Write action to be executed on flick here
 				}
 			}
 		}
