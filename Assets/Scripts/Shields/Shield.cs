e42727d73ef677ba87747e9809e92629eb86a5fe
Konrad <Joskony@users.noreply.github.com>
2018-11-17 16:41:15 +0100

    Old way of using special shield mechanic with keys

---
+++ Assets/Scripts/Shields/Shield.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+	private float m_positionTransitionSpeed = 0.05f;
+	[SerializeField] private Transform m_defaultPosition;
+	[SerializeField] private Transform m_extendedPosition;
+	[SerializeField] private Transform m_retractedPosition;
+	
+	public void MoveToDefault() {
+	
+		transform.position = Vector3.MoveTowards (transform.position, m_defaultPosition.position, m_positionTransitionSpeed);
+
+	}
+
+	public void MoveToExtended() {
+
+		transform.position = Vector3.MoveTowards (transform.position, m_extendedPosition.position, m_positionTransitionSpeed);
+
+	}
+
+	public void MoveToRetracted() {
+
+		transform.position = Vector3.MoveTowards (transform.position, m_retractedPosition.position, m_positionTransitionSpeed);
+
+	}
+
+}
--- Assets/Scripts/Shields/ShieldManager.cs
+++ Assets/Scripts/Shields/ShieldManager.cs
@@ -1,16 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Shields
 {
 	public class ShieldManager : MonoBehaviour
 	{
+
+		[SerializeField] private Shield[] m_shields;
+		private int m_shieldPositionGauge;
+		private int m_shieldPositionGaugeExtendedThreshold = 50;
+		private int m_shieldPositionGaugeRetractedThreshold = -50;
+		private enum ShieldPosition {Default, Extended, Retracted}
+		private ShieldPosition m_shieldPosition = ShieldPosition.Default;
+		
 		[SerializeField] private float m_rotationSpeed = 250.0f;
 		private float m_rotationDirection;
 		public float m_increasedRotationSpeed = 1.25f;
-	
+
+		private void Update()
+		{
+			MoveShieldsInput();
+		}
+
 		private void FixedUpdate()
 		{
 			RotateShields();
+			MoveShields();
 		}
 	
 		private void RotateShields()
@@ -21,6 +36,62 @@ namespace Shields
 		public void SetRotationDirection(float i)
 		{
 			m_rotationDirection = i;
+		}
+		
+		void MoveShields() {
+
+			if (m_shieldPositionGauge >= m_shieldPositionGaugeExtendedThreshold || m_shieldPositionGauge <= m_shieldPositionGaugeRetractedThreshold)
+				m_shieldPosition = ShieldPosition.Default;
+		
+			if (m_shieldPosition == ShieldPosition.Default)
+				foreach (Shield shield in m_shields)
+					shield.MoveToDefault ();
+			else if (m_shieldPosition == ShieldPosition.Extended)
+				foreach (Shield shield in m_shields)
+					shield.MoveToExtended ();
+			else if (m_shieldPosition == ShieldPosition.Retracted)
+				foreach (Shield shield in m_shields)
+					shield.MoveToRetracted ();
+
+		}
+		
+		private void MoveShieldsInput() {
+	
+			if (Input.GetKeyUp(KeyCode.Q) && m_shieldPositionGauge != m_shieldPositionGaugeExtendedThreshold) 
+			{
+				if (m_shieldPosition == ShieldPosition.Default || m_shieldPosition == ShieldPosition.Retracted) {
+					m_shieldPosition = ShieldPosition.Extended;
+					StartCoroutine (GainExtendedPoints ());
+				}
+				else if (m_shieldPosition == ShieldPosition.Extended)
+					m_shieldPosition = ShieldPosition.Default;
+			}
+			if (Input.GetKeyUp(KeyCode.E) && m_shieldPositionGauge != m_shieldPositionGaugeRetractedThreshold) 
+			{
+				if (m_shieldPosition == ShieldPosition.Default || m_shieldPosition == ShieldPosition.Extended) {
+					m_shieldPosition = ShieldPosition.Retracted;
+					StartCoroutine (GainRetractedPoints ());
+				}
+				else if (m_shieldPosition == ShieldPosition.Retracted)
+					m_shieldPosition = ShieldPosition.Default;
+			}
+
+		}
+		
+		private IEnumerator GainExtendedPoints() {
+			while (m_shieldPositionGauge <= m_shieldPositionGaugeExtendedThreshold && m_shieldPosition == ShieldPosition.Extended) {
+				m_shieldPositionGauge++;
+				yield return new WaitForSeconds (0.25f);
+			}
+
+		}
+
+		private IEnumerator GainRetractedPoints() {
+			while (m_shieldPositionGauge >= m_shieldPositionGaugeRetractedThreshold && m_shieldPosition == ShieldPosition.Retracted) {
+				m_shieldPositionGauge--;
+				yield return new WaitForSeconds (0.25f);
+			}
+
 		}
 	}
 }
