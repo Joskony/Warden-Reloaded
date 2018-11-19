4400be76fc1a029537a8e08f42ad16f2e415179a
Konrad Mampe <konradmampe@ws-246-169.dynamic.haw-hamburg.de>
2018-11-19 11:19:29 +0100

    Tags and projectile script

---
+++ Assets/Scripts/Enemies/Projectile/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private const float m_Speed = 2f;
+
+    private readonly Vector3 m_Target = Vector3.zero;
+
+    private void Update()
+    {
+        if (transform.position == m_Target)
+            gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        var step = m_Speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, m_Target, step);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Tags.M_BLOCKER_TAG) || other.gameObject.CompareTag(Tags.M_PLAYER_TAG) || other.gameObject.CompareTag(Tags.M_PROJECTILE_TAG))
+            gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/_Overhead/Tags.cs
@@ -0,0 +1,10 @@
+﻿public static class Tags {
+    // Player
+    public static string M_PLAYER_TAG = "Player";
+    
+    // Shield
+    public static string M_BLOCKER_TAG = "Blocker";
+    
+    // Projectiles
+    public static string M_PROJECTILE_TAG = "Projectile";
+}
