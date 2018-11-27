dd13d761e5558ef63a08661210517f035b77e5d3
Konrad <Joskony@users.noreply.github.com>
2018-11-27 21:27:39 +0100

    Player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using _Overhead;
+
+namespace Player
+{
+	public class Player : MonoBehaviour
+	{
+		public bool isAlive = true;
+		
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject.CompareTag(Tags.M_PROJECTILE_TAG))
+				isAlive = false;
+		}
+		
+	}
+}
+
+
